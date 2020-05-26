1587cbd18c868bce0e04dc42645f6fb702ad92b0
shubham tyagi <shubhamtyagi072@gmail.com>
2020-05-26 13:04:42 +0530

    Update Carousal.jsx
    fg

--- src/Component/Carousal/Carousal.jsx
+++ src/Component/Carousal/Carousal.jsx
@@ -29,13 +29,13 @@ setInterval((res) => {
        {this.state.value % 2 == 0 ? 
             <FlexGrid>
                 <FlexGrid.Row distribute="around">
-                    <FlexGrid.Col xs={12} lg={12} >
+                    <FlexGrid.Col xs={12} lg={12} xl={12} >
                         <Box vertical={7} between={1}>
                         <h1  style={{fontFamily:"algerian",color:"green",fontSize:"62px"}} >MAKING DELIVERY GREEN</h1>
                         <p style={{fontWeight:"bold",fontSize:"22px" }}>YOUR  BEST LAST MILE DELIVERY PARTNER</p>
                         </Box>
                     </FlexGrid.Col>
-                    <FlexGrid.Col xs={12} lg={12}>
+                    <FlexGrid.Col xs={12} lg={12} xl={12}>
                     <img style={{width:'80vw',margin:"auto"}} alt="Shubahm" src={Image} />
                     </FlexGrid.Col>
                 </FlexGrid.Row>
@@ -43,10 +43,11 @@ setInterval((res) => {
             <WrapperGradient>
           <FlexGrid>
           <FlexGrid.Row distribute="between" >
-              <FlexGrid.Col xs={12} lg={12}>
+              <FlexGrid.Col xs={12} lg={12} xl={12}>
               
                 <Box vertical={7} between={1}>
                 <h1  style={{fontFamily:"algerian",color:"green",fontSize:"62px"}} >YOUR BEST PARTNER</h1>
+                <p style={{fontWeight:"bold",fontSize:"22px" }}>We provide you cost effective and environment friendly solution for Last mile delivery</p>
                   </Box>
         
               </FlexGrid.Col>
@@ -54,16 +55,13 @@ setInterval((res) => {
              
           </FlexGrid.Row>
           <FlexGrid.Row distribute="between" >
-              <FlexGrid.Col xs={12} lg={4}>
+              <FlexGrid.Col xs={0} lg={2} xl={2}>
                 <div style={{zIndex:'100'}}>
-                <Box>
-                  <p style={{fontWeight:"bold",fontSize:"22px" }}>We provide you cost effective and environment friendly solution for Last mile delivery</p>
-                  </Box>
                 </div>
               </FlexGrid.Col>
-              <FlexGrid.Col xs={12} lg={8} >
+              <FlexGrid.Col xs={12} lg={10} xl={10} >
               <Box vertical={2} >
-                  <img   alt="Shubahm" src={Image2} />
+                  <img style={{width: '72vw',margin:"auto"}}  alt="Shubahm" src={Image2} />
                   </Box>
               </FlexGrid.Col>
              
